cae34bb28e37555f17417b2fd497e50c065d7a7a
Gnanesh <gnaneshkunal@outlook.com>
2025-11-06 17:49:58 +0530

    Blog index page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,31 +1,32 @@
 import Link from "next/link";
 import { getAllPosts } from "@/lib/org-parser";
 import { formatDateSimple } from "@/lib/blog-utils";
+import "./styles.css";
 
 export default async function BlogPage() {
   const posts = await getAllPosts();
 
   return (
-    <div>
-      <div>
-        <Link href="/">← Back</Link>
-        <h1>Logs</h1>
-        <p>Gnanesh&apos;s web logs</p>
+    <div className="blog-container">
+      <div className="blog-header">
+        <Link href="/" className="back-link">← Back</Link>
+        <h1>Gnanesh&apos;s web logs</h1>
       </div>
 
       {posts.length === 0 ? (
         <p>No logs yet.</p>
       ) : (
-        <div>
-          {posts.map((post) => (
+        <div className="blog-list">
+          {posts.map((post, index) => (
             <Link
               key={post.slug}
               href={`/blog/${post.slug}`}
+              className={`blog-item ${index % 2 === 0 ? 'even' : 'odd'}`}
             >
               <time dateTime={post.date}>
                 {formatDateSimple(post.date)}
               </time>
-              <span>{post.title}</span>
+              <span className="blog-title">{post.title}</span>
             </Link>
           ))}
         </div>
